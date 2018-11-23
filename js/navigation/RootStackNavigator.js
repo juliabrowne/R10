3e2e1bd0b73d5c5bb45d4c1c23ed4536b18da599
Julia Browne <julia.browne@live.ca>
2018-11-22 16:44:52 -0800

    add side navigation for android

--- js/navigation/RootStackNavigator.js
+++ js/navigation/RootStackNavigator.js
@@ -1,16 +1,5 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import AppNavigator from './NavigationLayout'
-// import AboutStack from './NavigationLayout'
-// import ScheduleStack from './NavigationLayout'
-// import MapStack from './NavigationLayout'
-// import FavesStack from './NavigationLayout'
 
-// const AppNavigator = createStackNavigator({
-//   About: AboutStack,
-//   Schedule: ScheduleStack,
-//   Map: MapStack,
-//   Faves: FavesStack
-// }, { headerMode: 'none' })
-// headerMode hides top nav bar!
 
 export default createAppContainer(AppNavigator)
