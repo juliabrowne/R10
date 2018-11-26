08427cdb1711b585543fdc24198f60fc17a7e8bc
Julia Browne <julia.browne@live.ca>
2018-11-25 22:15:13 -0800

    add styles to schedule screen

--- js/screens/Schedule/Schedule.js
+++ js/screens/Schedule/Schedule.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Text, View, ScrollView, Image, SectionList, Platform, TouchableHighlight, StatusBar } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import styles from './styles'
-import withNavigation from 'react-navigation'
+import { withNavigation } from 'react-navigation'
 import moment from 'moment'
 import PropTypes from 'prop-types'
 
@@ -22,7 +22,10 @@ const Schedule = props => {
                         > 
                             <View>
                                 <Text style={styles.sessionTitle}>{item.title}</Text>
+
+                            <View style={styles.sessionLocationContainer}>
                                 <Text style={styles.sessionLocation}>{item.location}</Text>
+                                {faveIds.find(fave => fave === item.id) && (
                                 <Ionicons
                                     name={Platform.select({
                                         ios: 'ios-heart',
@@ -30,13 +33,16 @@ const Schedule = props => {
                                     })}
                                     size={20}
                                     color={'red'}
+                                    style={{paddingRight: 20}}
                                 />
+                                )}
+                            </View>
                             </View>
                         </TouchableHighlight>
                     )}
                     
                     renderSectionHeader={({section}) => (
-                        <Text style={styles.headerText}>
+                        <Text style={styles.sessionTime}>
                             {moment(section.title).format('h:mm a')}
                         </Text>
                     )}
